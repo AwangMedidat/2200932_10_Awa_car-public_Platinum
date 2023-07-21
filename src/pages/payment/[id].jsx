d03d553c84a470084b9d175e89b4349c5271ba6c
Awang <awangmedidat98@gmail.com>
2023-07-22 00:59:44 +0700

    Add Auth in pages payment

--- src/pages/payment/[id].jsx
+++ src/pages/payment/[id].jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Payment from './Payment'
+import React from "react";
+import Payment from "./Payment";
 import { useRouter } from "next/router";
+import Auth from "@/components/Auth";
 
 function index() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   const { id } = router.query;
   return (
-    <Payment id={id}/>
-  )
+    <>
+      <Auth>
+        <Payment id={id} />
+      </Auth>
+    </>
+  );
 }
 
-export default index
+export default index;
